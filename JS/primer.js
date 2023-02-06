e81a1cfca0bf7ecb9ed69f438463e2ee2d2c1949
PacoBarni <paco12cv@hotmail.com>
2023-02-06 14:41:54 +0100

    js

--- JS/primer.js
+++ JS/primer.js
@@ -3,3 +3,7 @@ document.write("Hello world! escrito literal en el body");
 document.write(3);
 console.log("Hello world! en la consola");
 console.log(3);
+console.log('en', 'un', 'lugar', 'de', 'la', 'Mancha'); 
+console.info('Info');
+console.warn('Warning'); 
+console.error('Error');
---
+++ JS/variables.js
@@ -0,0 +1,15 @@
+var alert1 = 5;
+// permitido volver a declarar
+console.log(alert1);
+var alert1 = '';
+let alert2 = 3;
+console.log(alert2);
+// no permitido porque ya se declaró antes 
+// let alert2 = 5
+alert2=15;
+// cambio el valor pero no la vuelvo a declarar
+const alert3=13;
+// la constante no puede cambiar de valor
+console.log(alert1);
+// console.log(alert2);
+console.log(alert3);
