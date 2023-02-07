a950b621e53b1c4438b96bd1a414e66f3bbcafc9
PacoBarni <paco12cv@hotmail.com>
2023-02-07 12:53:10 +0100

    ej 3 pag 165 - conversiones

---
+++ JS/array.js
@@ -0,0 +1,15 @@
+const array1 = [];
+const array2 = [20, 3, 8];
+console.log(array2[1]);
+const array3 = ['Carmen', 'Juan'];
+// otra forma de declarar arrays 
+const array4 = new Array(20, 3, 8); // Equivalente a los corchetes
+console.log(array4[1]);
+const array5 = new Array(null, undefined, '', 8);
+console.log(array2[2]); //8
+array2[2]=13;
+console.log(array2[2]); //13
+console.log(array3[0]);
+console.log(array2.length);
+const date1 = new Date();
+console.log(date1);
---
+++ JS/ej3-165.js
@@ -0,0 +1,12 @@
+
+// De string a number
+const v2 = "3";
+console.log(parseInt(v2));
+
+// De number a string
+const v3 = 10;
+console.log(v3.toString());
+
+// De boolean a string
+const v4 = false;
+console.log(String(v4));
