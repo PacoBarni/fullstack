4b0bdb93b9e920fcaa5fe2cc929e60558f53f486
PacoBarni <paco12cv@hotmail.com>
2023-02-08 14:43:13 +0100

    bucles

---
+++ JS/contador.js
@@ -0,0 +1,5 @@
+let contador1 = 0;
+while (contador1 <= 5) { 
+contador1++;
+console.log(contador1);
+}
---
+++ JS/ej8b-171.js
@@ -0,0 +1,24 @@
+// Ejercicio 8 B
+
+for (let i = 50; i >= 1; i--) {
+console.log(i);
+}
+
+// Ejercicio 8 A
+
+for (let j = 1; j <= 50; j++) {
+    console.log(j);
+    }
+
+// Ejercicio 9
+
+for (let k = 0; k <= 10; k++) {
+    document.write("5 x" + k + " = " + k*5 + "<br>");
+}
+
+// Ejercicio 10
+
+let nombre = prompt("Escriba una palabra que será repetida 10 veces");
+    for (let a=0; a <10; a++);
+    console.log(i+1 + "." + nombre);
+
