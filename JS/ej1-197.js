fd58bfca5a2b026429c779643cbfa534d122d6d0
PacoBarni <paco12cv@hotmail.com>
2023-02-13 14:36:40 +0100

    13 feb

--- JS/ej1-197.js
+++ JS/ej1-197.js
@@ -1,17 +1,23 @@
-// const objeto = {
-//     name: "David Ravy",
-//     sclass: "VII",
-//     rollno: 12,
-// }
-// console.log(objeto.name);
-// console.log(objeto.sclass);
-// console.log(objeto.rollno);
+//
+const objeto = {
+    name: "David Ravy",
+    sclass: "VII",
+    rollno: 12,
+}
+console.log(objeto.name);
+console.log(objeto.sclass);
+console.log(objeto.rollno);
 
-// const objeto1 = {};
-// objeto1.name = "David Ravy";
-// objeto1.sclass = "VII";
-// objeto1.rollno = 12;
-// console.log(objeto1.name);
-// console.log(objeto1.sclass);
-// console.log(objeto1.rollno);
+const objeto1 = {};
+objeto1.name = "David Ravy";
+objeto1.sclass = "VII";
+objeto1.rollno = 12;
+console.log(objeto1.name);
+console.log(objeto1.sclass);
+console.log(objeto1.rollno);
 
+//
+const objeto2 = {
+    x: 1,
+    y: 2,
+}                                                               
--- JS/funciones.js
+++ JS/funciones.js
@@ -1,4 +1,70 @@
-const funcion1 = (sumar) => {
-return 2 + sumar;
-}
-console.log(funcion1);
+const sumar = (x,y) => {
+    return x+y;
+    };  
+console.log(sumar(1,3));
+
+const sumarSimple = (x,y) => x + y;
+console.log(sumarSimple(1,3));
+
+const sumarcall = (x,y,callback) => {
+    callback (x+y);
+    };
+sumarcall(1, 3, (resultado) => {
+    setTimeout (() => {
+        console.log(resultado);
+     },1000)
+ });    
+ 
+const restar = (x,y) => {
+    return x-y;
+    };
+console.log(restar(2,4));
+
+const restarSimple = (x,y) => x - y;
+console.log(restarSimple(2,4));
+
+const restarcall = (x,y,callback) => {
+    callback (x-y);
+    };
+restarcall(2, 4, (resultado) => {
+        setTimeout (() => {
+            console.log(resultado);
+         },2000)
+     });  
+
+const multiplicar = (x,y) => {
+    return x*y;
+    }
+console.log(multiplicar(3,6));
+
+const multiplicarSimple = (x,y) => x * y;
+console.log(multiplicarSimple(3,6));
+
+const multiplicarcall = (x,y,callback) => {
+    callback (x*y);
+    };
+multiplicarcall(3, 6, (resultado) => {
+        setTimeout (() => {
+        console.log(resultado);
+         },3000)
+     });  
+
+const dividir = (x,y) => {
+    return x/y;
+    }
+console.log(dividir(10,2));
+
+const dividirSimple = (x,y) => x / y;
+console.log(dividirSimple(10,2));
+
+const dividircall = (x,y,callback) => {
+    callback (x/y);
+    };
+dividircall(10, 2, (resultado) => {
+        setTimeout (() => {
+        console.log(resultado);
+         },4000)
+     });  
+
+
+
