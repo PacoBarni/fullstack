198dfbe98c83298fcd607f52b3c7b542cc03539c
PacoBarni <paco12cv@hotmail.com>
2023-02-13 12:45:44 +0100

    13 feb a

---
+++ JS/ej1-197.js
@@ -0,0 +1,17 @@
+// const objeto = {
+//     name: "David Ravy",
+//     sclass: "VII",
+//     rollno: 12,
+// }
+// console.log(objeto.name);
+// console.log(objeto.sclass);
+// console.log(objeto.rollno);
+
+// const objeto1 = {};
+// objeto1.name = "David Ravy";
+// objeto1.sclass = "VII";
+// objeto1.rollno = 12;
+// console.log(objeto1.name);
+// console.log(objeto1.sclass);
+// console.log(objeto1.rollno);
+
---
+++ JS/funciones.js
@@ -0,0 +1,4 @@
+const funcion1 = (sumar) => {
+return 2 + sumar;
+}
+console.log(funcion1);
